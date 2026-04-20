ee9a4072388002f33f09661bab3758c5a97692c6
Student <student@compass.local>
2026-04-20 10:38:06 +0600

    edit chat form

--- src/components/ChatForm/ChatForm.tsx
+++ src/components/ChatForm/ChatForm.tsx
@@ -1,7 +1,43 @@
+import {useState} from 'react';
+import {TextField, Button} from '@mui/material';
+import {axiosApi} from '../../axiosApi.ts';
+import styles from './styles.module.css';
+
 export const ChatForm = () => {
+  const [author, setAuthor] = useState('');
+  const [message, setMessage] = useState('');
+
+  const sendMessage = async () => {
+    if (!author.trim() || !message.trim()) return;
+
+    try {
+      await axiosApi.post('/messages.json', {
+        author,
+        message,
+      });
+
+      setAuthor('');
+      setMessage('');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div>
-      ChatForm
+      <TextField
+        label="Author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+
+      <TextField
+        label="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+
+      <Button onClick={sendMessage}>Send</Button>
     </div>
-  )
-}
+  );
+};
--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,9 +1,11 @@
+import { ChatForm } from '../../components/ChatForm/ChatForm.jsx';
 import {MessagesList} from '../../components/MessagesList/MessagesList.tsx';
 
 export const Chat = () => {
   return (
     <div>
-     <MessagesList/>
+      <ChatForm/>
+      <MessagesList/>
     </div>
   )
 }
